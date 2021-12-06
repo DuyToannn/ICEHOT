ce970a3489fb5dc1b647f22928b169ca8898ae58
Duy Toan <toanpmo123@gmail.com>
2021-12-05 19:12:03 -0700

    up

---
+++ popup sign up/main.js
@@ -0,0 +1,7 @@
+// Quick & dirty toggle to demonstrate modal toggle behavior
+$('.modal-toggle').on('click', function(e) {
+    e.preventDefault();
+    $('.modal').toggleClass('is-visible');
+  });
+
+ 
