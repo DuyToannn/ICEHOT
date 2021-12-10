81e4e3a5e33da9e279f4a244eb78764a4e50bca6
Duy Toan <toanpmo123@gmail.com>
2021-12-10 03:48:29 -0700

    up

---
+++ ICEHOT/assets/main/product.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".list-item-1 a").click(function() {
+        $('.list-item-kids-1 ').slideToggle();
+    })
+})
+$(document).ready(function() {
+    $(".list-item-2 a").click(function() {
+        $('.list-item-kids-2 ').slideToggle();
+    })
+})
