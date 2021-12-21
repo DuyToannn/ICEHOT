f9c5729cc9732ced037653e7f440884e7cae42c2
Duy Toan <toanpmo123@gmail.com>
2021-12-21 12:13:19 +0700

    up

--- ICEHOT/assets/main/home.js
+++ ICEHOT/assets/main/home.js
@@ -12,31 +12,31 @@ var swiper = new Swiper(".banner-swiper", {
 
 var swiper = new Swiper(".recipe-slider", {
     slidesPerView: 5,
-    spaceBetween: 30,
+ 
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: true,
-    },
+    // autoplay: {
+    //     delay: 4000,
+    //     disableOnInteraction: true,
+    // },
     breakpoints: {
         320: {
             slidesPerView: 2,
-            spaceBetween: 20
+       
         },
         480: {
             slidesPerView: 2,
-            spaceBetween: 30
+           
         },
         900: {
             slidesPerView: 3,
-            spaceBetween: 40
+         
         },
         1200: {
-            slidesPerView: 6,
-            spaceBetween: 40
+            slidesPerView: 5,
+            spaceBetween:-100
         }
 
     }
